1f96072df5baaacf2ecced6a95e1b7abba1b6543
ouk-ouk <35845424+ouk-ouk@users.noreply.github.com>
2021-02-25 00:23:02 +0100

    Mod name, description and such.

---
+++ tag-version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source_dir='./src/'
+info_file="${source_dir}info.json"
+
+cd "$(dirname "$0")"
+
+mod_version="$(jq -cr '.version' "$info_file")"
+
+git tag --annotate "$mod_version" --message="Version $mod_version"
