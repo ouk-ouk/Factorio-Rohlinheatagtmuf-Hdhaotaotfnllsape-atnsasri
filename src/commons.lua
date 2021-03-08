6680263ddcd8eb025b9749110fadf314da085356
ouk-ouk <35845424+ouk-ouk@users.noreply.github.com>
2021-03-09 00:08:34 +0100

    Allowed using all LUTs for every setting. Added more information and warnings to description of some settings.

--- src/commons.lua
+++ src/commons.lua
@@ -30,20 +30,20 @@ function makeSettingName(groupName, targetName, optionName)
 end
 
 colorSettingValues = {
-	identity =                {id = "identity",                lut = "identity",                                                               targetNames = {settingNames.targets.day, settingNames.targets.nightVision}},
-	vanilla_day =             {id = "vanilla_day",             lut = "__core__/graphics/color_luts/lut-day.png",                               targetNames = {settingNames.targets.day, settingNames.targets.nightVision}},
-	vanilla_night =           {id = "vanilla_night",           lut = "__core__/graphics/color_luts/lut-night.png",                             targetNames = {settingNames.targets.night, settingNames.targets.nightVision}},
-	vanilla_mapNight =        {id = "vanilla_mapNight",        lut = "__core__/graphics/color_luts/night.png",                                 targetNames = {settingNames.targets.night, settingNames.targets.nightVision}},
-	vanilla_nightVision =     {id = "vanilla_nightVision",     lut =  "__core__/graphics/color_luts/nightvision.png",                          targetNames = {settingNames.targets.nightVision}},
-	mod_grayNight =           {id = "mod_grayNight",           lut = "__" .. modName .. "__/graphics/color_luts/gray-night-lut.png",           targetNames = {settingNames.targets.night, settingNames.targets.nightVision}},
-	mod_blueNight =           {id = "mod_blueNight",           lut = "__" .. modName .. "__/graphics/color_luts/blue-night-lut.png",           targetNames = {settingNames.targets.night, settingNames.targets.nightVision}},
-	mod_darkNight =           {id = "mod_darkNight",           lut = "__" .. modName .. "__/graphics/color_luts/dark-night-lut.png",           targetNames = {settingNames.targets.night, settingNames.targets.nightVision}},
-	mod_grayDarkNight =       {id = "mod_grayDarkNight",       lut = "__" .. modName .. "__/graphics/color_luts/gray-dark-night-lut.png",      targetNames = {settingNames.targets.night, settingNames.targets.nightVision}},
-	mod_blueDarkNight =       {id = "mod_blueDarkNight",       lut = "__" .. modName .. "__/graphics/color_luts/blue-dark-night-lut.png",      targetNames = {settingNames.targets.night, settingNames.targets.nightVision}},
-	mod_black =               {id = "mod_black",               lut = "__" .. modName .. "__/graphics/color_luts/black-lut.png",                targetNames = {settingNames.targets.night, settingNames.targets.nightVision}},
-	mod_greenishNightVision = {id = "mod_greenishNightVision", lut = "__" .. modName .. "__/graphics/color_luts/greenish-nightvision-lut.png", targetNames = {settingNames.targets.nightVision}},
-	mod_grayNightVision =     {id = "mod_grayNightVision",     lut = "__" .. modName .. "__/graphics/color_luts/gray-nightvision-lut.png",     targetNames = {settingNames.targets.nightVision}},
-	mod_greenNightVision =    {id = "mod_greenNightVision",    lut = "__" .. modName .. "__/graphics/color_luts/green-nightvision-lut.png",    targetNames = {settingNames.targets.nightVision}},
+	identity =                {id = "identity",                lut = "identity"},
+	vanilla_day =             {id = "vanilla_day",             lut = "__core__/graphics/color_luts/lut-day.png"},
+	vanilla_night =           {id = "vanilla_night",           lut = "__core__/graphics/color_luts/lut-night.png"},
+	vanilla_mapNight =        {id = "vanilla_mapNight",        lut = "__core__/graphics/color_luts/night.png"},
+	vanilla_nightVision =     {id = "vanilla_nightVision",     lut =  "__core__/graphics/color_luts/nightvision.png"},
+	mod_grayNight =           {id = "mod_grayNight",           lut = "__" .. modName .. "__/graphics/color_luts/gray-night-lut.png"},
+	mod_blueNight =           {id = "mod_blueNight",           lut = "__" .. modName .. "__/graphics/color_luts/blue-night-lut.png"},
+	mod_darkNight =           {id = "mod_darkNight",           lut = "__" .. modName .. "__/graphics/color_luts/dark-night-lut.png"},
+	mod_grayDarkNight =       {id = "mod_grayDarkNight",       lut = "__" .. modName .. "__/graphics/color_luts/gray-dark-night-lut.png"},
+	mod_blueDarkNight =       {id = "mod_blueDarkNight",       lut = "__" .. modName .. "__/graphics/color_luts/blue-dark-night-lut.png"},
+	mod_black =               {id = "mod_black",               lut = "__" .. modName .. "__/graphics/color_luts/black-lut.png"},
+	mod_greenishNightVision = {id = "mod_greenishNightVision", lut = "__" .. modName .. "__/graphics/color_luts/greenish-nightvision-lut.png"},
+	mod_grayNightVision =     {id = "mod_grayNightVision",     lut = "__" .. modName .. "__/graphics/color_luts/gray-nightvision-lut.png"},
+	mod_greenNightVision =    {id = "mod_greenNightVision",    lut = "__" .. modName .. "__/graphics/color_luts/green-nightvision-lut.png"},
 }
 function colorSetting2lut(colorSetting)
 	for _, setting in pairs(colorSettingValues) do
--- src/settings.lua
+++ src/settings.lua
@@ -1,5 +1,6 @@
 require("commons")
 
+-- Constants & utils
 local localeNamePrefix = "mod-setting-name."
 local localeDescriptionPrefix = "mod-setting-description."
 local localeSettingNamePrefix = "mod-setting-name." .. settingNamePrefix
@@ -16,17 +17,10 @@ local function makeOrder(groupName, targetName, optionName)
 	return makeOrderPart(groupName, groupLetters) .. "-" .. makeOrderPart(optionName, optionLetters) .. "-" .. makeOrderPart(targetName, targetLetters)
 end
 
-local function makeAllowedColorValues(targetName)
-	local result = {}
-	for _, setting in pairs(colorSettingValues) do
-		for _, name in ipairs(setting.targetNames) do
-			if targetName == name then
-				table.insert(result, setting.id)
-				break
-			end
-		end
-	end
-	return result
+-- LUT settings
+local allowedColorValues = {}
+for _, setting in pairs(colorSettingValues) do
+	table.insert(allowedColorValues, setting.id)
 end
 
 local colorSettings = {
@@ -36,22 +30,33 @@ local colorSettings = {
 	{groupName = settingNames.groups.map,         default = colorSettingValues.mod_grayNight,        vanilla = colorSettingValues.vanilla_mapNight,    targetName = settingNames.targets.night},
 	{groupName = settingNames.groups.nightVision, default = colorSettingValues.mod_greenNightVision, vanilla = colorSettingValues.vanilla_nightVision, targetName = settingNames.targets.nightVision},
 }
+
 for _, setting in ipairs(colorSettings) do
 	optionName = settingNames.options.colors
 	data:extend({
 		{
 			type = "string-setting",
 			default_value = setting.default.id,
-			allowed_values = makeAllowedColorValues(setting.targetName),
+			allowed_values = allowedColorValues,
 			name = makeSettingName(setting.groupName, setting.targetName, optionName),
 			order = makeOrder(setting.groupName, setting.targetName, optionName),
 			setting_type = "startup",
-			localised_name = {localeNamePrefix .. makeSettingName(setting.groupName, "", optionName), {localeSettingNamePrefix .. setting.targetName}},
-			localised_description = {localeDescriptionPrefix .. makeSettingName(setting.groupName, "", optionName), {localeSettingDescriptionPrefix .. setting.targetName}, {localeSettingValueNamePrefix .. makeSettingName(setting.groupName, setting.targetName, optionName) .. "-" .. setting.default.id}, {localeSettingValueNamePrefix .. makeSettingName(setting.groupName, setting.targetName, optionName) .. "-" .. setting.vanilla.id}},
+			localised_name = {
+				localeNamePrefix .. makeSettingName(setting.groupName, "", optionName),
+				{localeSettingNamePrefix .. setting.targetName}
+			},
+			localised_description = {
+				localeDescriptionPrefix .. makeSettingName(setting.groupName, "", optionName),
+				{localeSettingDescriptionPrefix .. setting.targetName},
+				{localeSettingValueNamePrefix .. makeSettingName(setting.groupName, setting.targetName, optionName) .. "-" .. setting.default.id},
+				{localeSettingValueNamePrefix .. makeSettingName(setting.groupName, setting.targetName, optionName) .. "-" .. setting.vanilla.id},
+				setting.targetName == settingNames.targets.day and {localeDescriptionPrefix .. makeSettingName("darkDayWarning")} or ""
+			},
 		}
 	})
 end
 
+-- Percent settings
 local percentSettings = {
 	{groupName = settingNames.groups.game,        default = 15.0, vanilla = 25.0, targetName = settingNames.targets.sunset},
 	{groupName = settingNames.groups.game,        default = 30.0, vanilla = 10.0, targetName = settingNames.targets.night},
@@ -61,6 +66,7 @@ local percentSettings = {
 	{groupName = settingNames.groups.map,         default = 15.0, vanilla = 20.0, targetName = settingNames.targets.sunrise},
 	{groupName = settingNames.groups.nightVision, default = 70.0, vanilla = 50.0, targetName = settingNames.targets.nightVision},
 }
+
 for _, setting in ipairs(percentSettings) do
 	optionName = settingNames.options.percent
 	data:extend({
@@ -72,8 +78,15 @@ for _, setting in ipairs(percentSettings) do
 			name = makeSettingName(setting.groupName, setting.targetName, optionName),
 			order = makeOrder(setting.groupName, setting.targetName, optionName),
 			setting_type = "startup",
-			localised_name = {localeNamePrefix .. makeSettingName(setting.groupName, "", optionName), {localeSettingNamePrefix .. setting.targetName}},
-			localised_description = {localeDescriptionPrefix .. makeSettingName(setting.groupName, "", optionName), {localeSettingDescriptionPrefix .. setting.targetName}, setting.default, setting.vanilla},
+			localised_name = {
+				localeNamePrefix .. makeSettingName(setting.groupName, "", optionName),
+				{localeSettingNamePrefix .. setting.targetName}
+			},
+			localised_description = {
+				localeDescriptionPrefix .. makeSettingName(setting.groupName, "", optionName),
+				{localeSettingDescriptionPrefix .. setting.targetName},
+				setting.default, setting.vanilla
+			},
 		}
 	})
 end
